adb274745e0b54082d9416eee20671b6d0a04bf6
Clovis <shropshireclovis@gmail.com>
2017-11-17 00:09:00 -0500

    Added basic routes, controllers, and the admin area.

---
+++ SimpleMVCBlog/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleMVCBlog.Areas.Admin.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Admin/Ussers
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
